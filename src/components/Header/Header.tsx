f6d73d868c71ec7db80acc5df6ea631d387ad008
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 17:35:18 +0300

    Step 3 - Task 3.1: Created the Header component in the components/Header folder.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+// components/Header/Header.tsx
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      {/* Add header content here */}
+    </header>
+  );
+};
+
+export default Header;
