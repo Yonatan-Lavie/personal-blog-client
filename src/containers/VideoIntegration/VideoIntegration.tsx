49d01abc27a16fea55133bc350cdd8cf5841632d
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 18:39:13 +0300

    Step 4 - Task 4.7: Created VideoIntegration container in the containers/VideoIntegration folder. Implemented video-related features.

---
+++ src/containers/VideoIntegration/VideoIntegration.tsx
@@ -0,0 +1,18 @@
+// containers/VideoIntegration/VideoIntegration.tsx
+
+import React from 'react';
+import VideoFeature from '../components/VideoIntegration/VideoFeature';
+import VideoExplanation from '../components/VideoIntegration/VideoExplanation';
+
+const VideoIntegration: React.FC = () => {
+  return (
+    <div>
+      <h2>Video Integration</h2>
+      {/* Implement video-related features */}
+      <VideoFeature />
+      <VideoExplanation />
+    </div>
+  );
+};
+
+export default VideoIntegration;
