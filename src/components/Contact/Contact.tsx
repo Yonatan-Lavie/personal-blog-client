042947dd15d9ef067d6a92ec9878767c40faf360
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 17:42:33 +0300

    Step 3 - Task 3.9: Created the Contact component in the components/Contact folder.

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact: React.FC = () => {
+  return (
+    <div>
+      {/* Add contact content and contact form or email address here */}
+    </div>
+  );
+};
+
+export default Contact;
