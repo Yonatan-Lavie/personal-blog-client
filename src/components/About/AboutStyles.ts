d2c3df930692dad72b6cfc9d84190e326a7dca9c
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 18:00:29 +0300

    Step 3 - Task 10.8: Implemented styled-components for About component.

---
+++ src/components/About/AboutStyles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const AboutContainer = styled.div`
+  background-color: #ffffff;
+  padding: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const AboutTitle = styled.h2`
+  font-size: 24px;
+  color: #333;
+`;
+
+export const AboutContent = styled.p`
+  font-size: 16px;
+  color: #444;
+`;
