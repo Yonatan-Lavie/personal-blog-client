fcebc3df49017ca63a17a4568cc829d9286ff4a3
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 17:58:54 +0300

    Step 3 - Task 10.6: Implemented styled-components for GoldSubscription component.

---
+++ src/components/Subscription/GoldSubscriptionStyles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const GoldSubscriptionContainer = styled.div`
+  background-color: #f5f5f5;
+  padding: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const GoldSubscriptionTitle = styled.h2`
+  font-size: 24px;
+  color: #333;
+`;
+
+export const PaymentFormContainer = styled.div`
+  margin-top: 20px;
+`;
