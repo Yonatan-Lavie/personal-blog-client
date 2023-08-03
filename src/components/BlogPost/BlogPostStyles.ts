d63e52cd962e5fa6e1eac3de943df24f9e793978
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 17:56:32 +0300

    Step 3 - Task 10.3: Implemented styled-components for BlogPost component.

---
+++ src/components/BlogPost/BlogPostStyles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const BlogPostContainer = styled.article`
+  background-color: #ffffff;
+  padding: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 20px;
+`;
+
+export const Title = styled.h2`
+  font-size: 24px;
+  color: #333;
+`;
+
+export const DateAndAuthor = styled.div`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const Excerpt = styled.p`
+  font-size: 16px;
+  color: #444;
+`;
