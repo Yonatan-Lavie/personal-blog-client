71711ff387eb89add52eca749a98b3e3f7cea8cd
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 17:41:23 +0300

    Step 3 - Task 3.6: Created the GoldSubscription  component in the components/GoldSubscription folder.

---
+++ src/components/Subscription/GoldSubscription.tsx
@@ -0,0 +1,12 @@
+// components/Subscription/Subscription.tsx
+import React from 'react';
+
+const GoldSubscription: React.FC = () => {
+  return (
+    <div>
+      {/* Add subscription content and payment form here */}
+    </div>
+  );
+};
+
+export default GoldSubscription;
