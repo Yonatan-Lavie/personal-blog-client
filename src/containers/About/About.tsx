b338749571b08aea8f9b4e001c3047dc37b1578a
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 18:39:25 +0300

    Step 4 - Task 4.8: Created About container in the containers/About folder.

---
+++ src/containers/About/About.tsx
@@ -0,0 +1,18 @@
+// containers/About/About.tsx
+
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      {/* Add information about the project manager and UX professional */}
+      <p>About the Project Manager: [Your Name]</p>
+      <p>About the UX Professional: [UX Professional Name]</p>
+      {/* Share goals and motivations for creating this blog */}
+      <p>Goals and Motivations: [Your Goals and Motivations]</p>
+    </div>
+  );
+};
+
+export default About;
