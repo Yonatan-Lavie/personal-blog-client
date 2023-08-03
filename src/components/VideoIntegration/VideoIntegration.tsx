b42f22f17a6f071d1150643ce2fd51ab34b7c892
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 17:41:47 +0300

    Step 3 - Task 3.7: Created the VideoIntegration component in the components/VideoIntegration folder.

---
+++ src/components/VideoIntegration/VideoIntegration.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoIntegration: React.FC = () => {
+  return (
+    <div>
+      {/* Add video integration content here */}
+    </div>
+  );
+};
+
+export default VideoIntegration;
