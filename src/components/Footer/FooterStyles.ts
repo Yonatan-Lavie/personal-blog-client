4852d8b26a2a13924645e0838495b0b529624353
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 17:56:11 +0300

    Step 3 - Task 10.2: Implemented styled-components for Footer component.

---
+++ src/components/Footer/FooterStyles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  background-color: #f5f5f5;
+  padding: 16px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ContactLink = styled.a`
+  color: #007bff;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
