d82e8d280d196d48613b5f8fe62862c16f6d5daa
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 17:29:39 +0300

    Set up theme.ts file to define theme configurations and global styles using styled-components

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+// theme.ts
+const theme = {
+  colors: {
+    primary: '#007bff',
+    secondary: '#6c757d',
+    // Add more color variables as needed.
+  },
+  fonts: {
+    main: 'Arial, sans-serif',
+    // Add more font variables as needed.
+  },
+  breakpoints: {
+    mobile: '576px',
+    tablet: '768px',
+    desktop: '992px',
+    // Add more breakpoint variables as needed.
+  },
+};
+
+export default theme;
