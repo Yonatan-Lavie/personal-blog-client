e028dbc4b85a6d119a8f7fb4c833a072d6eb6ff2
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 18:38:43 +0300

    Step 4 - Task 4.6: Created GoldSubscription container in the containers/GoldSubscription folder. Implemented the payment form component.

---
+++ src/containers/GoldSubscription/GoldSubscription.tsx
@@ -0,0 +1,16 @@
+// containers/GoldSubscription/GoldSubscription.tsx
+
+import React from 'react';
+import PaymentForm from '../components/Subscription/PaymentForm';
+
+const GoldSubscription: React.FC = () => {
+  return (
+    <div>
+      <h2>Gold Subscription</h2>
+      {/* Implement the payment form for Gold subscription */}
+      <PaymentForm />
+    </div>
+  );
+};
+
+export default GoldSubscription;
