13182968ccf9f1b559d4896e9e3e11cdfd55f66e
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 18:37:32 +0300

    Step 4 - Task 4.4: Created UserAuthentication container in the containers/UserAuthentication folder. Implemented form components for sign-up and sign-in.

---
+++ src/containers/UserAuthentication/UserAuthentication.tsx
@@ -0,0 +1,30 @@
+// containers/UserAuthentication/UserAuthentication.tsx
+
+import React from 'react';
+import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
+import SignUpForm from '../forms/SignUpForm';
+import SignInForm from '../forms/SignInForm';
+
+const UserAuthentication: React.FC = () => {
+  const { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>User Authentication</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/signup`}>Sign Up</Link>
+        </li>
+        <li>
+          <Link to={`${url}/signin`}>Sign In</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path={`${path}/signup`} component={SignUpForm} />
+        <Route exact path={`${path}/signin`} component={SignInForm} />
+      </Switch>
+    </div>
+  );
+};
+
+export default UserAuthentication;
