4827d3ccce0539263e28ba0935b5e1260908f061
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 18:30:49 +0300

    Step 4 - Task 4.1: Created Home container in the containers/Home folder.

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,15 @@
+// containers/Home/Home.tsx
+
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      {/* Add content for the home page */}
+      <h1>Welcome to our Blog!</h1>
+      <p>Explore our amazing blog posts and discover exciting content.</p>
+    </div>
+  );
+};
+
+export default Home;
