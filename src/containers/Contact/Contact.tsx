becd4d3d2bc8a389b99786d3c8fa9e6d48534d45
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 18:39:39 +0300

    Step 4 - Task 4.9: Created Contact container in the containers/Contact folder. Implemented the contact form component.

---
+++ src/containers/Contact/Contact.tsx
@@ -0,0 +1,16 @@
+// containers/Contact/Contact.tsx
+
+import React from 'react';
+import ContactForm from '../components/Footer/ContactForm';
+
+const Contact: React.FC = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      {/* Implement the contact form */}
+      <ContactForm />
+    </div>
+  );
+};
+
+export default Contact;
