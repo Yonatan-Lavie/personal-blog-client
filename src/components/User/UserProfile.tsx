17aeb999a5bb57ea0e6ed225dca57d9a6fc56b87
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 17:39:34 +0300

    Step 3 - Task 3.5: Created the UserProfile component in the components/UserProfile folder.

---
+++ src/components/User/UserProfile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserProfile: React.FC = () => {
+  return (
+    <div>
+      {/* Add user profile content here */}
+    </div>
+  );
+};
+
+export default UserProfile;
