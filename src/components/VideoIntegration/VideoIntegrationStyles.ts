a06bbd0eb49f8d42f52bf4c8cf85200d67e24eab
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-03 18:00:05 +0300

    Step 3 - Task 10.7: Implemented styled-components for VideoIntegration component.

---
+++ src/components/VideoIntegration/VideoIntegrationStyles.ts
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const VideoIntegrationContainer = styled.div`
+  background-color: #ffffff;
+  padding: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const VideoFeatureTitle = styled.h3`
+  font-size: 20px;
+  color: #333;
+  margin: 0;
+`;
+
+export const VideoFeatureDescription = styled.p`
+  font-size: 16px;
+  color: #444;
+`;
+
+export const VideoExplanation = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+export const VideoThumbnail = styled.img`
+  width: 150px;
+  height: 100px;
+  object-fit: cover;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const VideoDescription = styled.div`
+  margin-left: 10px;
+`;
+
+export const VideoTitle = styled.h4`
+  font-size: 18px;
+  color: #333;
+  margin: 0;
+`;
+
+export const VideoAuthor = styled.p`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const VideoViews = styled.p`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const VideoLikes = styled.p`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const VideoShareButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 4px;
+  cursor: pointer;
+`;
